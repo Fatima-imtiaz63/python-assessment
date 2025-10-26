b2562d52ebe034d09e5f02d0fe4f5642893b410a
Fatima <fimtaiz63@gmail.com>
2025-10-26 15:33:37 +0000

    initial commit

---
+++ practice.py
@@ -0,0 +1,23 @@
+#sum = 0
+#for i in range(1, 11):
+ #   sum = sum +i
+   # print("adding:", i,"current sum:",sum)
+    #print("The total sum is:", sum)
+
+#print multiplication table of 5
+#number = 5
+#print("Multiplication table of", 5)
+#for i in range(1, 11):
+ ## print(number, "x", i, "=", number*i)
+
+  #print multiplication of table 6
+  #number = 6
+  #print("multiplication of table", 6)
+  # i in range(1, 11):
+    #(number, "x", i,"=" , number*i) 
+
+    #print table of any number entered by the user
+number = int (input("Enter any number: "))
+print("multiplication of table", number)
+for i in range(1 ,11):
+    print(number, "x", i, "=" ,number*i)
